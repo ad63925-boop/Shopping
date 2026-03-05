86d888ae63a7c8d28eee6be22b3fe9aad4867c33
ad63925-boop <ad63925@gmail.com>
2026-03-05 11:47:48 +0200

    Отображение ошибки showError(message)

--- script.js
+++ script.js
@@ -166,6 +166,31 @@ function isValidItemName(name) {
     return true;
 }
 
+//Функция отображения ошибки
+function showError(message) {
+    // Удаляем предыдущее сообщение, если есть
+    const existingError = document.getElementById('inputError');
+    if (existingError) {
+        existingError.remove();
+    }
+
+    const nameInput = document.getElementById('itemName');
+    const errorDiv = document.createElement('div');
+    errorDiv.id = 'inputError';
+    errorDiv.className = 'input-error';
+    errorDiv.textContent = message;
+
+    // Вставляем сообщение сразу после поля ввода
+    nameInput.parentNode.insertBefore(errorDiv, nameInput.nextSibling);
+
+    // Автоматически скрываем через 5 секунд
+    setTimeout(() => {
+        if (errorDiv.parentNode) {
+            errorDiv.remove();
+        }
+    }, 5000);
+}
+
 
 // Функция выбора подсказки
 function selectSuggestion(name, cat, price, quantity) {
