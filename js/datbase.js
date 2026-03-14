04aa44a7dbca1b64af736a84f0c6ae00217aa545
ad63925-boop <ad63925@gmail.com>
2026-03-14 14:54:12 +0200

    Обновил

--- js/datbase.js
+++ js/datbase.js
@@ -1,4 +1,1 @@
-//Связь с облаком
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database().ref("shopping_list");
-const settingsDb = firebase.database().ref("settings"); // Для сохранения лимита
+
